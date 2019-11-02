d730eb1e6da61882cc9b83a7b511d6e01acda644
Sxy623 <191436989@qq.com>
2019-11-02 12:03:05 +0800

    Diffuse materials

--- main.cpp
+++ main.cpp
@@ -6,10 +6,19 @@
 #include <cfloat>
 using namespace std;
 
+vec3 random_in_unit_sphere() {
+    vec3 p;
+    do {
+        p = 2.0 * vec3(random_double(), random_double(), random_double()) - vec3(1, 1, 1);
+    } while (p.squared_length() >= 1.0);
+    return p;
+}
+
 vec3 color(const ray &r, hittable *world) {
     hit_record rec;
     if (world->hit(r, 0.0, FLT_MAX, rec)) {
-        return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * color(ray(rec.p, target - rec.p), world);
     }
     else {
         vec3 unit_direction = unit_vector(r.direction());
@@ -47,6 +56,7 @@ int main() {
                 col += color(r, world);
             }
             col /= ns;
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
